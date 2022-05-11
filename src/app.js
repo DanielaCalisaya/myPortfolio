f718bbdd68908f6445972cae3dd0b4dec2005aa9
DanielaCalisaya <hdaniela127c@gmail.com>
2022-05-11 18:39:39 -0500

    entrypoint

--- src/app.js
+++ src/app.js
@@ -1,2 +1,22 @@
 const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3030;
 
+app.use(express.static('public'));
+
+app.use("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+//routes
+let homePath = require('./routers/home');
+
+//home
+app.use('/', homePath);
+
+
+
+
+app.listen(PORT, () => 
+console.log(`Servidor levantado en el puerto ${PORT}
+->  http://localhost:${PORT}`))
