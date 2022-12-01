a31478e49af43ab728a76b73528641db3a624760
DanielaCalisaya <hdaniela127c@gmail.com>
2022-12-01 18:09:43 -0300

    added component NabBar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,15 @@
+import NabBar from './components/NabBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>
           edit
         </p>
-        
-      </header>
+      </header> */}
+      <NabBar/>
     </div>
   );
 }
